a7a62c8ddb6e2c3ec881849389dbb55a2bce8b06
sage <danasmufasa@gmail.com>
2018-12-13 01:59:56 +0100

    Just in...

--- dg/blog/urls.py
+++ dg/blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+	url(r'^/posts/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 	url(r'^$', views.post_list, name='post_list'),
-	url(r'^(?P<pk>[0-9]+)/post_detail', views.post_detail, name='post_detail'),
 ]
 
 
