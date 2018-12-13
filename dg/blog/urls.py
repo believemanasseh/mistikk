eee034f89eaffa67cac2c8effe905d23f55e587a
sage <danasmufasa@gmail.com>
2018-12-13 01:16:03 +0100

    Just in...

--- dg/blog/urls.py
+++ dg/blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    url('', views.post_list, name='post_list'),
-	url('(?P<pk>[0-9]+)/post_detail', views.post_detail, name='post_detail'),
+    url(r'^$', views.post_list, name='post_list'),
+	url(r'^(?P<pk>[0-9]+)/post_detail', views.post_detail, name='post_detail'),
 ]
 
 
--- dg/dg/urls.py
+++ dg/dg/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-	url('', include('blog.urls')),
+	url(r'^$', include('blog.urls')),
 ]
