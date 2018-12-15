f1e2af238eff37ffa4fd241b82cafd73455460c0
sage <danasmufasa@gmail.com>
2018-12-15 16:26:42 +0100

    New...

--- dg/blog/urls.py
+++ dg/blog/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+	url(r'^posts/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
+	url(r'^posts/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
+	url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
 	url(r'^posts/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 	url(r'^posts/new/$', views.post_new, name='post_new'),
 	url(r'^posts/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
